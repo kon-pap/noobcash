bb8f009dd67a2227c8e2712ef1a0c40b2738a9ef
billothi <billtheo309@hotmail.com>
2022-01-10 16:27:51 +0200

    Added isValidSig in node

--- pkg/node/node.go
+++ pkg/node/node.go
@@ -1,6 +1,11 @@
 package node
 
-import bck "github.com/kon-pap/noobcash/pkg/node/backend"
+import (
+	"crypto"
+	"crypto/rsa"
+
+	bck "github.com/kon-pap/noobcash/pkg/node/backend"
+)
 
 type Node struct {
 	// chain, currBlockId, wallet, ring
@@ -20,11 +25,20 @@ func NewNode(currBlockId int, bits int) *Node {
 	}
 }
 
-/*
 func (n *Node) IsValidSig(tx bck.Transaction) bool {
+
+	err := rsa.VerifyPKCS1v15(tx.SenderAddress, crypto.SHA256, tx.Id, tx.Signature)
+	return err == nil
 }
+
+/*
 func (n *Node) IsValidTx(tx bck.Transaction) bool {
+	//The validation is consisted of 2 steps
+	//Step1: isValidSig
+	//Step2: check transaction inputs/outputs
+	isvalidSig := n.IsValidSig(tx) //Step1
 }
+
 func (n *Node) MineBlock(block *bck.Block) error {
 }
 func (n *Node) BroadcastBlock(block *bck.Block) error {
